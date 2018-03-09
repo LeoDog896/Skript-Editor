8ef6a3e145c0a33ffafd4dd5c0d9036ff832c33c
Glitch (skript-editor) <none>
2018-03-09 01:44:27 +0000

    🚌🚿 Checkpoint index.html:629503/37 script.js:629503/359

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ editor.setValue("command /example:\n" +
                 "    trigger:\n        message \"This is an example message" + 
                 "\"");
 editor.clearSelection();
+$('#export').click(function(){
+  create('skript.sk',editor.getValue())
+});
+$('#import').click(function(){
+  
+});
+function create(filename, data) {
+    var blob = new Blob([data], {type: 'text/csv'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        var elem = document.getElementById('export');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;        
+    }
+}
