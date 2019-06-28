58310ec01ea10c51fb31c4920052826dad0039af
Glitch (skript-editor) <none>
2019-06-28 14:35:06 +0000

    📒🏹 Checkpoint ./public/scripts/script.js:629503/920

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,40 @@
 /*
 
 */
-/* global ace, Mode, Cookies */
+/* global ace, Mode, Cookies, define */
+define('ace/mode/skript', function(require, exports, module) {
+
+var oop = require("ace/lib/oop");
+var TextMode = require("ace/mode/text").Mode;
+var ExampleHighlightRules = require("ace/mode/example_highlight_rules").ExampleHighlightRules;
+
+var Mode = function() {
+    this.HighlightRules = ExampleHighlightRules;
+};
+oop.inherits(Mode, TextMode);
+
+(function() {
+    // Extra logic goes here. (see below)
+}).call(Mode.prototype);
+
+exports.Mode = Mode;
+});
+
+define('ace/mode/skript_highlight_rules', function(require, exports, module) {
+
+var oop = require("ace/lib/oop");
+var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+
+var ExampleHighlightRules = function() {
+
+    this.$rules = new TextHighlightRules().getRules();
+    
+}
+
+oop.inherits(ExampleHighlightRules, TextHighlightRules);
+
+exports.ExampleHighlightRules = ExampleHighlightRules;
+});
 if (location.protocol != "https:") location.protocol = "https:";
 function byteCount(s) {
     return encodeURI(s).split(/%..|./).length - 1;
