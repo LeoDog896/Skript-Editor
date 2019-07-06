f38b2941266c98f9859f9ead2ae6d95c2d976037
Glitch (skript-editor) <none>
2019-07-06 20:34:01 +0000

    😻🐔 Checkpoint ./views/app.html:629503/1134 ./public/scripts/script.js:629503/1200

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -110,36 +110,5 @@ $(".file").click(function(){
   }
 })
 
-$("#customize").click(() => $(".modal")[0].classList.toggle("show-modal"))
-$(".close-button").click(() => document.querySelector(".modal").classList.toggle("show-modal"))
-var reader = new FileReader();
-reader.addEventListener("loadend", function() {
-   editor.setValue(reader.result)
-});
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
-});
+$("#customize").click(() => $(".modal").addClass("show-modal"))
+$(".close-button").click(() => $(".modal").removeClass("show-modal"))
