8bc6fe92f25196bfe8c6c22c82b5a979639ec31d
Glitch (skript-editor) <none>
2019-07-07 18:32:47 +0000

    👶🚠 Checkpoint ./public/ace/mode-python.js:629503/159463

--- public/ace/mode-python.js
+++ public/ace/mode-python.js
@@ -1,5 +1,38 @@
-define("ace/mode/python_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"],function(e,t,n){
-  "use strict";
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2010, Ajax.org B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Ajax.org B.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+/*
+ * TODO: python delimiters
+ */
+
+define("ace/mode/python_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
+"use strict";
 
 var oop = require("../lib/oop");
 var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
@@ -7,15 +40,15 @@ var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 var PythonHighlightRules = function() {
 
     var keywords = (
-        "if|trigger|description|command|permission"
+        "and|if|else if|else"
     );
 
     var builtinConstants = (
-        "true|false|player|loop-player|victim|loop-victim|attacker|loop-attacker|arg 1|arg 2|arg 3| arg 4"
+        "true|false"
     );
 
     var builtinFunctions = (
-        "abs|message"
+        "abs|message|send|teleport"
     );
 
     //var futureReserved = "";
@@ -120,12 +153,6 @@ var PythonHighlightRules = function() {
         }, {
             include: "constants"
         }],
-        "qqstring3": [{
-            token: "constant.language.escape",
-            regex: stringEscape
-        }, {
-            defaultToken: "string"
-        }],
         "qqstring": [{
             token: "constant.language.escape",
             regex: stringEscape
@@ -173,16 +200,6 @@ var PythonHighlightRules = function() {
             token: "string",
             regex: "'|$",
             next: "start"
-        }, {
-            defaultToken: "string"
-        }],
-        "fqqstring3": [{
-            token: "constant.language.escape",
-            regex: stringEscape
-        }, {
-            token: "paren.lparen",
-            regex: "{",
-            push: "fqstringParRules"
         }, {
             defaultToken: "string"
         }],
@@ -300,11 +317,3 @@ oop.inherits(PythonHighlightRules, TextHighlightRules);
 
 exports.PythonHighlightRules = PythonHighlightRules;
 });
-(function() {
-window.require(["ace/mode/python"], function(m) {
-                        if (typeof module == "object" && typeof exports == "object" && module) {
-                            module.exports = m;
-                        }
-                    });
-                })();
-            
