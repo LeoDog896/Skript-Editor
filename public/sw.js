449deeba022396c8d8443594f2a12863e143b287
Glitch (skript-editor) <none>
2019-07-15 19:59:13 +0000

    😎📇 Checkpoint ./views/app.html:629503/16 ./public/sw.js:629503/1201

--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,28 @@ var CACHE_NAME = 'version_01'
 // Add a path you want to cache in this list.
 var URLS = [                
   '/',
-  '/index.html',
-  '/style.css',
-  '/app.js'
+  '/app',
+  'ace/ace.js',
+  'lib/codeblast.js',
+  'ace/ext-split.js',
+  'ace/ext-language_tools.js',
+  'scripts/script.js',
+  'styles/main.css',
+  'images/hash.svg',
+  'images/brush.svg',
+  'images/download.svg',
+  'images/file.svg',
+  'images/logo.svg',
+  'images/options.svg',
+  'images/refresh.svg',
+  'images/upload.svg',
+  'ace/mode-skript.js',
+  'manifest.json',
+  'https://fonts.googleapis.com/css?family=Inconsolata',
+  'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js',
+  'https://fonts.gstatic.com/s/inconsolata/v17/QldKNThLqRwH-OJ1UHjlKGlZ5qg.woff2',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'
 ]
 console.log(navigator)
 console.log(navigator.geolocation)
