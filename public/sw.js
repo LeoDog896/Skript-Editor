50589411fac842a7116eb0f06cee0c1091fd7fce
Glitch (skript-editor) <none>
2019-07-17 13:06:00 +0000

    🍬🌅 Checkpoint ./public/sw.js:629503/16 ./public/styles/main.css:629503/82

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Version of the offline cache (change this value everytime you want to update cache)
-var CACHE_NAME = 've01'              
+var CACHE_NAME = 'skript_ver'              
 
 // Add a path you want to cache in this list.
 var URLS = [                
