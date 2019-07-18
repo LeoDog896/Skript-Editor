a1b92baf374c10c4d38357d6a19f47412d9f120a
Glitch (skript-editor) <none>
2019-07-18 12:54:51 +0000

    😜🌊 Checkpoint ./server.js:629503/310 ./views/app.html:629503/394 ./public/styles/main.css:629503/43

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express');
 var compression = require('compression')
 const app = express();
 app.use(express.static('public'));
-app.use(compression())
+app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/app.html'));
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
