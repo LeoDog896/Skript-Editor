6aeb1fe3c4f1ca2ad031fa7b8671d042b4daac11
Glitch (skript-editor) <none>
2019-07-28 01:44:34 +0000

    ⚾️🏃 Checkpoint ./views/embed.html:629503/13058 ./server.js:629503/103 ./public/styles/index.css:629503/4 ./public/styles/main.css:629503/194 ./mainEmbed:629503/2361 ./public/styles/mainEmbed.css:629503/6830

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ app.use(compression())
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/app.html'));
+app.get('/embed', (req, res) => res.sendFile(__dirname + '/views/embed.html'))
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
