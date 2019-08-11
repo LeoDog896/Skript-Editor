31e414a3384098f7e13dff494f1f4dbcea464ea2
Glitch (skript-editor) <none>
2019-08-11 18:12:05 +0000

    💰🌻 Checkpoint ./server.js:629503/130 ./package.json:629503/1471

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-var compression = require('compression')
-var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const express         = require('express');
+const compression     = require('compression')
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const helmet          = require('helmet')
 const app = express();
-app.use(redirectToHTTPS())
-app.use(compression())
+
+app.use(redirectToHTTPS());
+app.use(compression());
 app.use(express.static('public'));
+app.use(helmet());
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/app.html'));
 app.get('/embed', (req, res) => res.sendFile(__dirname + '/views/embed.html'))
+
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
