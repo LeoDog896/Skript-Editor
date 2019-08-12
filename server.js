14b8e7387fecefb5b86cdf0c472653443962879a
Glitch (skript-editor) <none>
2019-08-12 16:06:42 +0000

    🚨🗾 Checkpoint ./package.json:629503/3099 ./server.js:629503/751 ./public/styles/markdown.css:629503/6832 ./views/app.html:629503/351 ./LICENSE.md:629503/2 ./views/api.html:629503/1983

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express         = require('express');
 const compression     = require('compression')
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 const helmet          = require('helmet')
-const app = express();
+const fs              = require('fs').promises;
+const marked          = require('marked')
+const sanitizeHtml    = require('sanitize-html');
+const app             = express();
 
 app.use(redirectToHTTPS());
 app.use(compression());
@@ -12,5 +15,8 @@ app.use(helmet());
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/app.html'));
 app.get('/embed', (req, res) => res.sendFile(__dirname + '/views/embed.html'))
+
+app.get('/license', async (request, response) => response.send(sanitizeHtml(marked.parse(await fs.readFile('LICENSE.md', 'utf8'))) + '<style scoped> @import url("/styles/markdown.css");</style>'))
+app.get('/api', async (request, response) => response.send(sanitizeHtml(marked.parse(await fs.readFile('API.md', 'utf8'))) + '<style scoped> @import url("/styles/markdown.css");</style>'))
 
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
