131e18398104b1c02ba326d391459689d7f1052c
Glitch (skript-editor) <none>
2019-08-12 19:04:19 +0000

    🚂❣️ Checkpoint ./views/raw.html:629503/248 ./server.js:629503/95

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/app.html'));
 app.get('/embed', (req, res) => res.sendFile(__dirname + '/views/embed.html'))
 app.get('/alone', (req, res) => res.sendFile(__dirname + '/views/alone.html'))
+app.get('/raw', (req, res) => res.sendFile(__dirname + '/views/raw.html'))
 
 app.get('/license', async (request, response) => response.send(await markdown.buildFile('LICENSE.md', {title: "skLicense", desc: "License for Skript Editor", style: "/styles/markdown.css"})))
 app.get('/api', async (request, response) => response.send(await markdown.buildFile('API.md', {title: "skAPI", desc: "API for Skript Editor", style: "/styles/markdown.css"})))
