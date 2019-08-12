e3ca461a5a3cba5f4f0d32b54710dc42791589b3
Glitch (skript-editor) <none>
2019-08-12 17:14:53 +0000

    👏👖 Checkpoint ./markdown.js:629503/1108 ./server.js:629503/316

--- markdown.js
+++ markdown.js
@@ -2,7 +2,12 @@ const fs              = require('fs').promises;
 const marked          = require('marked')
 const sanitizeHtml    = require('sanitize-html');
 class Markdown {
-  buildText async (txt, options) => {
+  constructor() {
+    this.buildText = this.buildText.bind(this)
+    this.buildFile = this.buildFile.bind(this)
+  }
+  
+  async buildText (txt, options) {
     return `
 <!DOCTYPE html>
 <html>
@@ -19,35 +24,10 @@ class Markdown {
   </body>
 </html>
 `
-  },
+  }
   
-  buildFile: async (file, options) => {
-    console.log(await fs.readFile(file, 'utf8'))
-    return this.buildText(await fs.readFile(file, 'utf8'), options)
+  async buildFile (file, options) {
+    return await this.buildText(await fs.readFile(file, 'utf8'), options)
   }
 }
-module.exports = {
-  buildText: async (txt, options) => {
-    return `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="description" content="${options.desc}"/>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1>
-    <title>${options.title}</title>
-  </head>
-  <body>
-    ${sanitizeHtml(marked.parse(txt))}
-    <style scoped> @import url("${options.style}");</style>
-  </body>
-</html>
-`
-  },
-  
-  buildFile: async (file, options) => {
-    console.log(await fs.readFile(file, 'utf8'))
-    return this.buildText(await fs.readFile(file, 'utf8'), options)
-  }
-}
+module.exports = new Markdown()
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/app.html'));
 app.get('/embed', (req, res) => res.sendFile(__dirname + '/views/embed.html'))
 
-app.get('/license', async (request, response) => response.send(markdown.buildFile('LICENSE.md', {title: "skLicense", desc: "License for Skript Editor", style: "/styles/markdown.css"})))
-app.get('/api', async (request, response) => response.send(markdown.buildFile('API.md', {title: "skAPI", desc: "API for Skript Editor", style: "/styles/markdown.css"})))
+app.get('/license', (request, response) => console.log(markdown.buildFile('LICENSE.md', {title: "skLicense", desc: "License for Skript Editor", style: "/styles/markdown.css"})))
+app.get('/api', (request, response) => response.send(markdown.buildFile('API.md', {title: "skAPI", desc: "API for Skript Editor", style: "/styles/markdown.css"})))
 
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
