23b2ac284086b531ac98a759b4fdae7199932eb1
Glitch (skript-editor) <none>
2019-08-13 23:47:27 +0000

    ✊👷 Checkpoint ./tiny.js:629503/61

--- tiny.js
+++ tiny.js
@@ -1,10 +1,8 @@
-function makeid(length) {
+let makeid = length => {
   var result           = '';
-  var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+  for ( var i = 0; i < length; i++ ) result += characters.charAt(Math.floor(Math.random() * charactersLength));
   return result;
 }
 
