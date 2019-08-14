86ddd33c1280189a09b16e2a0680f21fd880f1ee
Glitch (skript-editor) <none>
2019-08-14 22:38:10 +0000

    🍗🐊 Checkpoint ./public/scripts/multi.js:629503/52 ./public/scripts/script.js:629503/52

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -191,7 +191,7 @@ $("#autocomplete-o").change(() => {
 })
 $("#soft-s").change(() => editor.setOption("tabSize", $("#soft-s").val()))
 $(".close-button").click(() => $(".modal").removeClass("show-modal"))
-$("#discord").click(() => window.open("https://discord.gg/ukEwAAC"))
+$("#discord").click(() => window.open("https://discord.gg/y9ENcnz"))
 
 
 // Block stuff
