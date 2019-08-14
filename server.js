cc5dc1b686074be9d132fb90cbb7757a6671f2d8
Glitch (skript-editor) <none>
2019-08-14 14:49:59 +0000

    🌇📠 Checkpoint ./views/index.html:629503/448:3382547/66 ./server.js:629503/886 ./CONTRIBUTION.md:629503/45:3382547/306 ./package.json:629503/1285

--- server.js
+++ server.js
@@ -31,12 +31,15 @@ app.route('/shorturl').post((req, res) => {
   }
   humans.push(tim);
   retrieveHumans[tim] = req.body.data
-  app.get("/" + tim, (req, res) => res.redirect("/app#" + retrieveHumans[req.url.substring(1)]))
-  res.json({url: tim, data: req.body.data})
+  app.get("/s/" + tim, (req, res) => res.redirect("/app#" + retrieveHumans[req.url.substring(1)]))
+  res.json({url: "s/" + tim, data: req.body.data})
 }).get((req, res) => res.json({error: "Wrong Method"}))
 
 app.get('/license', async (request, response) => response.send(await markdown.buildFile('LICENSE.md', {title: "skLicense", desc: "License for Skript Editor", style: "/styles/markdown.css"})))
 app.get('/api', async (request, response) => response.send(await markdown.buildFile('API.md', {title: "skAPI", desc: "API for Skript Editor", style: "/styles/markdown.css"})))
 app.get('/code_of_conduct', async (request, response) => response.send(await markdown.buildFile('CODE_OF_CONDUCT.md', {title: "skCOC", desc: "Code of Conduct for skript editor", style: "/styles/markdown.css"})))
+app.get('/contributors', async (request, response) => response.send(await markdown.buildFile('CONTRIBUTION.md', {title: "Contribution", desc: "Users who contributed to skEditor", style: "/styles/markdown.css"})))
+app.get('/contribution', async (request, response) => response.send(await markdown.buildFile('CONTRIBUTION.md', {title: "Contribution", desc: "Users who contributed to skEditor", style: "/styles/markdown.css"})))
+
 
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
