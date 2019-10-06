8e1e91591238630a0d4f8ab82b3e5f75645edbc0
Glitch (skript-editor) <none>
2019-10-06 20:10:15 +0000

    🚔🌚 Checkpoint ./public/scripts/script.js:629503/126

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,7 +4,6 @@ setInterval(() => isReadyShort = true, 5000)
 console.log(location.hash)
 let byteCount = s => encodeURI(s).split(/%..|./).length - 1;
 let errorRegs = [
-  { reg: /\s+(command|function) .+:/g, msg: 'Declerations of commands & functions should not have whitespaces behind them' },
   { reg: /^(trigger|description|cooldown|permission)([/\w]+|):([/\w+]|)/g, msg: 'Command properties should have whitespaces behind them' },
   { reg: /teleport (the |)(player|attacker|victim|loop-entity|loop-player|) (to|below|above|next to) (-|)\d+(,|) (-|)\d+(,|) (-|)\d+/g, msg: "Use vector(x, y, z) instead of x, y, z" },
   { reg: /^{_\w+}/g, msg: 'You cant use temp variables unless its in an event/command!' },
