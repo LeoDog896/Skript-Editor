900de57c08e4d309b313c9b6d98940f3f1e5cdae
Glitch (skript-editor) <none>
2020-01-03 21:43:11 +0000

    🏚🚪 Checkpoint ./tiny.js:629503/608 ./ace.js:629503/28

--- tiny.js
+++ tiny.js
@@ -1,9 +1,10 @@
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const charLength = characters.length;
 module.exports = length => {
-  let result = '';
+  let result = "";
   let i;
   for (i = 0; i < length; i++)
     result += characters.charAt(Math.floor(Math.random() * charLength));
   return result;
-}
+};
