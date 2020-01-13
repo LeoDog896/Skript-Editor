4ec08646ee61ea8693db169c6f589be57e1362b4
Glitch (skript-editor) <none>
2020-01-13 18:43:08 +0000

    🏝🎷 Checkpoint ./public/scripts/script.js:629503/14516 ./public/robots.txt:629503/302

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,22 @@
 /* global ace, Mode, Cookies, define, LZString, codeBlastAce, Toast */
 let isReadyShort = true;
-setInterval(() => isReadyShort = true, 5000)
+setInterval(() => (isReadyShort = true), 5000);
 let byteCount = s => encodeURI(s).split(/%..|./).length - 1;
 let errorRegs = [
-  { reg: /^{_\w+}/g, msg: 'You cant use temp variables unless its in an event/command!' },
-]
+  {
+    reg: /^{_\w+}/g,
+    msg: "You cant use temp variables unless its in an event/command!"
+  }
+];
 
 function create(filename, text) {
-  let element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
+  let element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
@@ -18,28 +24,31 @@ function create(filename, text) {
 
 function copyTextToClipboard(text) {
   if (!navigator.clipboard) return;
-  navigator.clipboard.writeText(text)
+  navigator.clipboard.writeText(text);
 }
 
 window.isUpdateAvailable = new Promise(function(resolve, reject) {
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/sw.js').then(reg => {
-		  reg.onupdatefound = () => {
-			  const installingWorker = reg.installing;
-				installingWorker.onstatechange = () => {
-				switch (installingWorker.state) {
-					case 'installed':
-						if (navigator.serviceWorker.controller) resolve(true)
-            else resolve(false);
-						break;
-					}
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(reg => {
+        reg.onupdatefound = () => {
+          const installingWorker = reg.installing;
+          installingWorker.onstatechange = () => {
+            switch (installingWorker.state) {
+              case "installed":
+                if (navigator.serviceWorker.controller) resolve(true);
+                else resolve(false);
+                break;
+            }
+          };
         };
-			};
-		}).catch(err => console.error('[SW ERROR]', err));
-	}
+      })
+      .catch(err => console.error("[SW ERROR]", err));
+  }
 });
 var editor = ace.edit("editor");
-codeBlastAce(ace)
+codeBlastAce(ace);
 editor.setShowPrintMargin(false);
 editor.session.setMode("ace/mode/skript");
 editor.setValue(`command /hello:
@@ -57,89 +66,111 @@ editor.setOptions({
   enableLiveAutocompletion: true
 });
 
-var langTools = ace.require('ace/ext/language_tools');
-$('#export').click(() => create('skript.sk',editor.getValue()));
+var langTools = ace.require("ace/ext/language_tools");
+$("#export").click(() => create("skript.sk", editor.getValue()));
 $("#theme").change(() => {
-  editor.setTheme("ace/theme/" + $("#theme").val())
-  Cookies.set('theme', $("#theme").val())
-})
+  editor.setTheme("ace/theme/" + $("#theme").val());
+  Cookies.set("theme", $("#theme").val());
+});
 var temp;
-setTimeout(() => window.parseReady = true, 2000);
-editor.getSession().on('change', function(e) {
+setTimeout(() => (window.parseReady = true), 2000);
+editor.getSession().on("change", function(e) {
   temp = e;
-  $("#bytes").html(byteCount(editor.getValue()))
+  $("#bytes").html(byteCount(editor.getValue()));
   $("#lines").html(editor.getValue().split(/\r\n|\r|\n/).length);
-  Cookies.set('data',editor.getValue());
+  Cookies.set("data", editor.getValue());
   if (window.parseReady) {
-    editor.getSession().setAnnotations([])
+    editor.getSession().setAnnotations([]);
     if (editor.getValue() == "") {
-      editor.getSession().setAnnotations([{
-        row: 0,
-        column: 0,
-        text: "File is empty",
-        type: "warning"
-      }]);
+      editor.getSession().setAnnotations([
+        {
+          row: 0,
+          column: 0,
+          text: "File is empty",
+          type: "warning"
+        }
+      ]);
     } else {
       let annData = [];
-      editor.getValue().split("\n").forEach((txt, index) => {
-        errorRegs.forEach(data => {
-          if (txt.match(data.reg)) annData.push({row: index, column: 0, text: data.msg, type: data.type ? data.type : "error"})
-        })
-      })
-      editor.getSession().setAnnotations(annData)
+      editor
+        .getValue()
+        .split("\n")
+        .forEach((txt, index) => {
+          errorRegs.forEach(data => {
+            if (txt.match(data.reg))
+              annData.push({
+                row: index,
+                column: 0,
+                text: data.msg,
+                type: data.type ? data.type : "error"
+              });
+          });
+        });
+      editor.getSession().setAnnotations(annData);
     }
   }
 });
 
-$(() => {
-  window['isUpdateAvailable'].then(isAvailable => {
-		if (isAvailable) {
-			new Toast({message: "A new update is available! Refresh to update, or force refresh (CTRL + F5) for a forced update!"})
-		}
-	});
-  $('body').on('dragover', function(e) {
+window["isUpdateAvailable"].then(isAvailable => {
+  if (isAvailable) {
+    new Toast({
+      message:
+        "A new update is available! Refresh to update, or force refresh (CTRL + F5) for a forced update!"
+    });
+  }
+});
+$("body").on("dragover", function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $("#editor").css("filter", "blur(4px)");
+});
+$("body").on("dragenter", function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+});
+$("body").on("dragleave", function() {
+  $("#editor").css("filter", "blur(0px)");
+});
+$("body").on("drop", function(e) {
+  if (
+    e.originalEvent.dataTransfer &&
+    e.originalEvent.dataTransfer.files.length
+  ) {
+    $("#editor").css("filter", "blur(0px)");
     e.preventDefault();
     e.stopPropagation();
-    $("#editor").css("filter", "blur(4px)")
-  })
-  $('body').on('dragenter', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-  })
-  $("body").on('dragleave', function() {
-    $("#editor").css("filter", "blur(0px)")
-  })
-  $('body').on('drop', function(e){
-    if (e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
-      $("#editor").css("filter", "blur(0px)")
-      e.preventDefault();
-      e.stopPropagation();
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        var text = e.target.result;
-        editor.setValue(text)
-      }
-      reader.readAsText(e.originalEvent.dataTransfer.files[0]);
-    }
-  });
-  if (Cookies.get('data') && !location.hash) editor.setValue(Cookies.get('data'));
-  if (Cookies.get('theme')) editor.setTheme("ace/theme/" + Cookies.get('theme'))
-  setTimeout(() => {
-    try {
-      Cookies.get("blastCode") ? editor.setOption('blastCode', { effect: 1 }) : editor._codeBlast.destroy()
-    } catch (e) {
-      
-    }
-  }, 200)
-  if (Cookies.get("autocomplete")) editor.setOption("enableLiveAutocompletion", Cookies.get('autocomplete'))
-  if (location.hash) editor.setValue(LZString.decompressFromBase64(decodeURI(location.hash.substring(1))))
-  editor.clearSelection();
-  $(`[value=${editor.getTheme().replace("ace/theme/","")}]`).prop('selected', true);
-})
-$("#bytes").html(byteCount(editor.getValue()))
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var text = e.target.result;
+      editor.setValue(text);
+    };
+    reader.readAsText(e.originalEvent.dataTransfer.files[0]);
+  }
+});
+if (Cookies.get("data") && !location.hash) editor.setValue(Cookies.get("data"));
+if (Cookies.get("theme")) editor.setTheme("ace/theme/" + Cookies.get("theme"));
+setTimeout(() => {
+  try {
+    Cookies.get("blastCode")
+      ? editor.setOption("blastCode", { effect: 1 })
+      : editor._codeBlast.destroy();
+  } catch (e) {}
+}, 200);
+if (Cookies.get("autocomplete"))
+  editor.setOption("enableLiveAutocompletion", Cookies.get("autocomplete"));
+if (location.hash)
+  editor.setValue(
+    LZString.decompressFromBase64(decodeURI(location.hash.substring(1)))
+  );
+editor.clearSelection();
+$(`[value=${editor.getTheme().replace("ace/theme/", "")}]`).prop(
+  "selected",
+  true
+);
+$("#bytes").html(byteCount(editor.getValue()));
 $("#lines").html(editor.getValue().split(/\r\n|\r|\n/).length);
-$('#import').click(() => document.getElementById("fileElem").click());
-$("#fileElem").change(function(e){
+$("#import").click(() => document.getElementById("fileElem").click());
+$("#fileElem").change(function(e) {
   let tempFile = e.target.files[0];
   var reader = new FileReader();
   reader.readAsText(tempFile, "UTF-8");
@@ -147,11 +178,11 @@ $("#fileElem").change(function(e){
 });
 $("#link").click(() => {
   if (!isReadyShort) return;
-  isReadyShort = false
+  isReadyShort = false;
   $.ajax({
     type: "POST",
     url: "/shorturl",
-    data: {data: LZString.compressToBase64(editor.getValue())},
+    data: { data: LZString.compressToBase64(editor.getValue()) },
     success: function(data) {
       let toaster = new Toast({
         message: "Link: https://skript-editor.glitch.me/" + data.url,
@@ -160,23 +191,25 @@ $("#link").click(() => {
           {
             text: "Copy",
             onClick: function() {
-              copyTextToClipboard("https://skript-editor.glitch.me/" + data.url)
-              toaster._close()
+              copyTextToClipboard(
+                "https://skript-editor.glitch.me/" + data.url
+              );
+              toaster._close();
             }
           }
         ]
-      })
+      });
     }
   });
-})
+});
 
 $("#share").click(() => {
   if (!isReadyShort) return;
-  isReadyShort = false
+  isReadyShort = false;
   $.ajax({
     type: "POST",
     url: "/shareurl",
-    data: {data: LZString.compressToBase64(editor.getValue())},
+    data: { data: LZString.compressToBase64(editor.getValue()) },
     success: function(data) {
       let toaster = new Toast({
         message: "Link: https://skript-editor.glitch.me/" + data.url,
@@ -185,25 +218,36 @@ $("#share").click(() => {
           {
             text: "Copy",
             onClick: function() {
-              copyTextToClipboard("https://skript-editor.glitch.me/" + data.url)
-              toaster._close()
+              copyTextToClipboard(
+                "https://skript-editor.glitch.me/" + data.url
+              );
+              toaster._close();
             }
           }
         ]
-      })
+      });
     }
   });
-})
+});
 
-$("#options").click(() => $(".options-modal").addClass("show-modal"))
+$("#options").click(() => $(".options-modal").addClass("show-modal"));
 $("#blast-o").change(() => {
-  $("#blast-o").is(':checked') ? editor.setOption('blastCode', { effect: 1 }) : editor._codeBlast.destroy()
-  Cookies.set('blastCode', $("#blast-o").is(':checked'))
-})
-$("#soft-o").change(() => $("#soft-o").is(':checked') ? editor.setOption('useSoftTabs', true) : editor.setOption('useSoftTabs', false))
+  $("#blast-o").is(":checked")
+    ? editor.setOption("blastCode", { effect: 1 })
+    : editor._codeBlast.destroy();
+  Cookies.set("blastCode", $("#blast-o").is(":checked"));
+});
+$("#soft-o").change(() =>
+  $("#soft-o").is(":checked")
+    ? editor.setOption("useSoftTabs", true)
+    : editor.setOption("useSoftTabs", false)
+);
 $("#autocomplete-o").change(() => {
-  editor.setOption("enableLiveAutocompletion", $("#autocomplete-o").is(':checked'))
-  Cookies.set('autocomplete', $("#autocomplete-o").is(':checked'))
-})
-$("#soft-s").change(() => editor.setOption("tabSize", $("#soft-s").val()))
-$(".close-button").click(() => $(".modal").removeClass("show-modal"))
+  editor.setOption(
+    "enableLiveAutocompletion",
+    $("#autocomplete-o").is(":checked")
+  );
+  Cookies.set("autocomplete", $("#autocomplete-o").is(":checked"));
+});
+$("#soft-s").change(() => editor.setOption("tabSize", $("#soft-s").val()));
+$(".close-button").click(() => $(".modal").removeClass("show-modal"));
